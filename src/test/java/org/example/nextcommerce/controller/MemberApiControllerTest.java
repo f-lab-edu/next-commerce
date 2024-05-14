dc92b71b456e0e0bac7d5af90b11ca97f5d94064
codenee <tanedoran@gmail.com>
2024-05-14 17:20:02 +0900

    [#4] memberController 테스트코드, mockMVC적용

---
+++ src/test/java/org/example/nextcommerce/controller/MemberApiControllerTest.java
@@ -0,0 +1,59 @@
+package org.example.nextcommerce.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.nextcommerce.dto.MemberDto;
+import org.example.nextcommerce.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class MemberApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private MemberDto memberDto;
+
+    @BeforeEach
+    void setUp(){
+        memberDto = MemberDto.builder()
+                .email("nextcommerce@naver.com")
+                .password("asdf123!")
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    public void successCreatePost() throws Exception {
+
+        String content = objectMapper.writeValueAsString(memberDto);
+
+        mockMvc.perform(post("/api/members")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+
+}
