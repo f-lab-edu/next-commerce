e06cc3129693850e424315964f2d6b7bc9e8be54
codenee <tanedoran@gmail.com>
2024-06-18 12:12:48 +0900

    [#13]cartDto 인스턴스 변수 추가(quantity)

--- src/main/java/org/example/nextcommerce/cart/dto/CartDto.java
+++ src/main/java/org/example/nextcommerce/cart/dto/CartDto.java
@@ -11,11 +11,14 @@ public class CartDto {
     private Long cartId;
     private Long memberId;
     private Long postId;
+    private Long imageId;
     private Integer quantity;
 
-    public CartDto(Long memberId, Long postId, Integer quantity){
+
+    public CartDto(Long memberId, Long postId,  Long imageId, Integer quantity){
         this.memberId = memberId;
         this.postId = postId;
+        this.imageId = imageId;
         this.quantity = quantity;
     }
 
