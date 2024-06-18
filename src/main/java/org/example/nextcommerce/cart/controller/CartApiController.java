3089bb183b63fd1cf6f9656439fceb6b14ab3dbe
codenee <tanedoran@gmail.com>
2024-06-18 12:14:05 +0900

    [#13]장바구니 이미지, 목록 조회 컨트롤러 추가

--- src/main/java/org/example/nextcommerce/cart/controller/CartApiController.java
+++ src/main/java/org/example/nextcommerce/cart/controller/CartApiController.java
@@ -9,8 +9,11 @@ import org.example.nextcommerce.common.annotation.LoginMember;
 import org.example.nextcommerce.common.annotation.LoginRequired;
 import org.example.nextcommerce.member.dto.MemberDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -26,6 +29,27 @@ public class CartApiController {
         cartService.save(memberDto.getId(), cartRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+
+
+    @LoginRequired
+    @GetMapping(value = "/image/{postId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> imageFile(@PathVariable Long postId){
+
+        byte[] bytes = cartService.getImageFileTop1(postId);
+
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.IMAGE_JPEG).body(bytes);
+    }
+
+    @LoginRequired
+    @GetMapping()
+    public ResponseEntity<List<CartDto>> cartListALl(@LoginMember MemberDto memberDto){
+
+        List<CartDto> cartDtoList = cartService.getCartListAll(memberDto.getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(cartDtoList);
+
+    }
+
 
 
 
