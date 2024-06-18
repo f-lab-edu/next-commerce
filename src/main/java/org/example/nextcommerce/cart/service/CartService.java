6589ee1a9f562cb1a09ea5324e90e9323aee1a8c
codenee <tanedoran@gmail.com>
2024-06-18 12:19:25 +0900

    [#13]장바구니 서비스에 이미지파일 조회, 장바구니 목록 조회 기능 추가

--- src/main/java/org/example/nextcommerce/cart/service/CartService.java
+++ src/main/java/org/example/nextcommerce/cart/service/CartService.java
@@ -3,6 +3,10 @@ package org.example.nextcommerce.cart.service;
 import org.example.nextcommerce.cart.dto.CartDto;
 import org.example.nextcommerce.cart.dto.CartRequestDto;
 
+import java.util.List;
+
 public interface CartService {
     public void save(Long memberId,CartRequestDto cartRequestDto);
+    public byte[] getImageFileTop1(Long postId);
+    public List<CartDto> getCartListAll(Long memberId);
 }
--- src/main/java/org/example/nextcommerce/cart/service/CartServiceImpl.java
+++ src/main/java/org/example/nextcommerce/cart/service/CartServiceImpl.java
@@ -5,7 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.nextcommerce.cart.dto.CartDto;
 import org.example.nextcommerce.cart.dto.CartRequestDto;
 import org.example.nextcommerce.cart.repository.jdbc.CartJdbcRepository;
+import org.example.nextcommerce.common.exception.NotFoundException;
+import org.example.nextcommerce.common.utils.errormessage.ErrorCode;
+import org.example.nextcommerce.post.dto.ImageDto;
+import org.example.nextcommerce.post.repository.jdbc.ImageJdbcRepository;
+import org.example.nextcommerce.post.service.ImageFileService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,14 +21,40 @@ import org.springframework.stereotype.Service;
 public class CartServiceImpl implements  CartService{
 
     private final CartJdbcRepository cartJdbcRepository;
+    private final ImageFileService imageFileService;
+    private final ImageJdbcRepository imageJdbcRepository;
 
+
+    @Transactional
     @Override
     public void save(Long memberId, CartRequestDto cartRequestDto) {
+
+        ImageDto imageDto = imageJdbcRepository.findTop1ByPostId(cartRequestDto.getPostId());
+
         CartDto cartDto = CartDto.builder()
                 .memberId(memberId)
                 .postId(cartRequestDto.getPostId())
                 .quantity(cartRequestDto.getQuantity())
+                .imageId(imageDto.getImageId())
                 .build();
         cartJdbcRepository.save(cartDto);
     }
+
+    @Override
+    public byte[] getImageFileTop1(Long postId) {
+
+        ImageDto imageDto = imageJdbcRepository.findTop1ByPostId(postId);
+
+        if(!imageFileService.validImageFile(imageDto.getFilePath())){
+            throw new NotFoundException(ErrorCode.ImageFileNotFound);
+        }
+        return imageFileService.downloadImageFile(imageDto.getFilePath());
+    }
+
+    @Override
+    public List<CartDto> getCartListAll(Long memberId) {
+
+        return cartJdbcRepository.findAllByMemberId(memberId);
+    }
 }
+
