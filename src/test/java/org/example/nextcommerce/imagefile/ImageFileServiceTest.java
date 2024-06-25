87d63e37cd2df391ff59f3283f62bb693bcdaded
codenee <tanedoran@gmail.com>
2024-06-25 16:20:40 +0900

    [#13]ImageFileService테스트 코드 작성

---
+++ src/test/java/org/example/nextcommerce/imagefile/ImageFileServiceTest.java
@@ -0,0 +1,82 @@
+package org.example.nextcommerce.imagefile;
+
+import org.example.nextcommerce.common.exception.FileHandleException;
+import org.example.nextcommerce.common.exception.UnauthorizedException;
+import org.example.nextcommerce.post.service.ImageFileService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.isDirectory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class ImageFileServiceTest {
+
+    @Autowired
+    private ImageFileService imageFileService;
+
+    @Test
+    @DisplayName("파일 경로 유효성 실패 테스트")
+    public void failValidImageFileNoPath(){
+        String imagePath = "E:\\false";
+        assertFalse(imageFileService.validImageFile(imagePath));
+    }
+
+    @Test
+    @DisplayName("파일 유효성 실패 테스트")
+    public void failValidImageFileNotExistFile(){
+        String imagePath = "E:\\false.jpeg";
+        assertFalse(imageFileService.validImageFile(imagePath));
+
+    }
+
+    @Test
+    @DisplayName("파일 다운로드 성공 테스트")
+    public void successFileDownload(){
+        //given
+        Path filePath = Paths.get("src","main","resources","test.jpg");
+        //when
+        byte[] bytes = imageFileService.downloadImageFile(filePath.toString());
+        //then
+        assertThat(bytes).isNotEmpty();
+        assertEquals("src\\main\\resources\\test.jpg", filePath.toString());
+    }
+
+    @Test
+    @DisplayName("파일 다운로드 실패 테스트")
+    public void failFileDownload(){
+        //given
+        ClassPathResource classPathResource = new ClassPathResource("/testfail.jpg");
+
+        //when-then
+        assertThrows(FileHandleException.class, ()->{
+            imageFileService.downloadImageFile(classPathResource.getPath());
+        });
+    }
+
+    @Test
+    @DisplayName("모든 파일(디레토리 포함) 삭제 실패 테스트")
+    public void failDeleteDirectoryAll(){
+        //given
+        Path filePath = Paths.get("src","testDir","deleteDir");
+        assertThrows(FileHandleException.class, ()->{
+            imageFileService.deleteDirectoryAll(filePath.toString());
+        });
+    }
+
+}
