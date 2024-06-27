fda6484d9871e5e042962204c8d362c04041099f
codenee <tanedoran@gmail.com>
2024-06-27 14:51:02 +0900

    [#29]Jdbc사용 로직->jpa서비스 클래스로 수정

--- src/main/java/org/example/nextcommerce/common/interceptor/LoginMemberInterceptor.java
+++ src/main/java/org/example/nextcommerce/common/interceptor/LoginMemberInterceptor.java
@@ -5,21 +5,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.nextcommerce.common.annotation.LoginMember;
+import org.example.nextcommerce.common.exception.MemberNotFoundException;
 import org.example.nextcommerce.common.exception.UnauthorizedException;
 import org.example.nextcommerce.common.utils.errormessage.ErrorCode;
 import org.example.nextcommerce.member.dto.MemberDto;
+import org.example.nextcommerce.member.entity.Member;
 import org.example.nextcommerce.member.repository.jdbc.MemberJdbcRepository;
+import org.example.nextcommerce.member.repository.jpa.MemberJpaRepository;
+import org.example.nextcommerce.member.service.MemberJpaService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.lang.reflect.Parameter;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class LoginMemberInterceptor implements HandlerInterceptor {
 
-    private final MemberJdbcRepository memberJdbcRepository;
+    private final MemberJpaService memberJpaService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -34,7 +39,7 @@ public class LoginMemberInterceptor implements HandlerInterceptor {
                     if (memberId == null){
                         throw new UnauthorizedException(ErrorCode.SessionIsNull);
                     }
-                    MemberDto memberDto = memberJdbcRepository.findByMemberId(memberId);
+                    MemberDto memberDto = memberJpaService.checkLoginMember(memberId);
                     request.setAttribute("MemberDto", memberDto);
                 }
             }
