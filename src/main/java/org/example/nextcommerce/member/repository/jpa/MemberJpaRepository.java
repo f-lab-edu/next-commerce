4535f48e9060e39e6b48216f75649e593f239c39
codenee <tanedoran@gmail.com>
2024-06-27 14:32:20 +0900

    [#29]이메일로 Member엔티티 DB 조회하는 메소드 추가

--- src/main/java/org/example/nextcommerce/member/repository/jpa/MemberJpaRepository.java
+++ src/main/java/org/example/nextcommerce/member/repository/jpa/MemberJpaRepository.java
@@ -3,9 +3,12 @@ package org.example.nextcommerce.member.repository.jpa;
 import org.example.nextcommerce.member.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+import java.util.Optional;
+
+public interface MemberJpaRepository extends CrudRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+    public Optional<Member> findMemberByEmail(String email);
 
 
 }
