e480129eea93dc3560de75a8fd6043ac454f8987
codenee <tanedoran@gmail.com>
2024-06-30 23:12:38 +0900

    [#29]Cart컨트롤러 테스트 코드 수정(Jdbc->Jpa)

--- src/test/java/org/example/nextcommerce/cart/controller/CartApiControllerTest.java
+++ src/test/java/org/example/nextcommerce/cart/controller/CartApiControllerTest.java
@@ -2,14 +2,22 @@ package org.example.nextcommerce.cart.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.nextcommerce.cart.dto.CartRequestDto;
+import org.example.nextcommerce.cart.entity.Cart;
 import org.example.nextcommerce.cart.repository.jdbc.CartJdbcRepository;
+import org.example.nextcommerce.cart.repository.jpa.CartJpaRepository;
 import org.example.nextcommerce.cart.service.CartService;
+import org.example.nextcommerce.common.resolver.LoginMemberArgumentResolver;
+import org.example.nextcommerce.image.repository.jpa.ImageJpaRepository;
 import org.example.nextcommerce.member.dto.MemberDto;
 import org.example.nextcommerce.member.repository.jdbc.MemberJdbcRepository;
-import org.example.nextcommerce.member.service.MemberService;
+import org.example.nextcommerce.member.repository.jpa.MemberJpaRepository;
+import org.example.nextcommerce.member.service.MemberJdbcService;
 import org.example.nextcommerce.image.repository.jdbc.ImageJdbcRepository;
+import org.example.nextcommerce.member.service.MemberJpaService;
+import org.example.nextcommerce.post.controller.PostApiControllerTest;
 import org.example.nextcommerce.post.repository.jdbc.PostJdbcRepository;
 import org.example.nextcommerce.image.service.ImageFileService;
+import org.example.nextcommerce.post.repository.jpa.PostJpaRepository;
 import org.example.nextcommerce.post.service.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,16 +26,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,31 +52,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(CartApiController.class)
 public class CartApiControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @MockBean
-    private MemberService memberService;
+    private MemberJpaService memberJpaService;
+    //private MemberJdbcService memberService;
+    @MockBean
+    private MemberJpaRepository memberJpaRepository;
+    //private MemberJdbcRepository memberJdbcRepository;
+
 
     @MockBean
     private PostService postService;
 
     @MockBean
-    private MemberJdbcRepository memberJdbcRepository;
-
-    @MockBean
-    private CartJdbcRepository cartJdbcRepository;
+    private CartJpaRepository cartJpaRepository;
+    //private CartJdbcRepository cartJdbcRepository;
 
     @MockBean
     private ImageFileService imageFileService;
 
     @MockBean
-    private ImageJdbcRepository imageJdbcRepository;
+    private ImageJpaRepository imageJpaRepository;
+    //private ImageJdbcRepository imageJdbcRepository;
 
     @MockBean
-    private PostJdbcRepository postJdbcRepository;
+    private PostJpaRepository postJpaRepository;
+    //private PostJdbcRepository postJdbcRepository;
 
 
     @MockBean
@@ -69,17 +88,42 @@ public class CartApiControllerTest {
 
     private MockHttpSession mockHttpSession;
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CartApiController cartApiController;
+
+    private MockManagerArgumentResolver mockManagerArgumentResolver = new MockManagerArgumentResolver();
+
+    static class MockManagerArgumentResolver extends LoginMemberArgumentResolver{
+        @Override
+        public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+            MemberDto memberDto = MemberDto.builder()
+                    .id(1L)
+                    .email("test@naver.com")
+                    .password("asdf7890*")
+                    .build();
+            return memberDto;
+        }
+    }
+
 
     @BeforeEach
     void setUp(){
+
+        mockMvc = MockMvcBuilders.standaloneSetup(cartApiController)
+                .setControllerAdvice(new ExceptionHandlerExceptionResolver())
+                .setCustomArgumentResolvers(mockManagerArgumentResolver)
+                .build();
+
         MemberDto memberDto = MemberDto.builder()
-                        .id(999L)
+                        .id(1L)
                         .email("test@naver.com")
                         .password("asdf7890*")
                         .build();
 
         mockHttpSession = new MockHttpSession();
-        mockHttpSession.setAttribute("MemberId", 999L);
+        mockHttpSession.setAttribute("MemberId", 1L);
         mockHttpSession.setAttribute("MemberDto", memberDto);
 
         cartRequestDto = CartRequestDto.builder()
@@ -106,8 +150,13 @@ public class CartApiControllerTest {
     @DisplayName("장바구니 조회 성공 테스트")
     @Test
     public void successCartListAll() throws Exception{
-        given(cartService.getCartListAll(any())).willReturn(List.of());
-        mockMvc.perform(get("/api/cart").contentType(MediaType.APPLICATION_JSON))
+        //given
+        given(cartService.getCartListAll(anyLong())).willReturn(List.of());
+
+        //then
+        mockMvc.perform(get("/api/cart")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(mockHttpSession))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
