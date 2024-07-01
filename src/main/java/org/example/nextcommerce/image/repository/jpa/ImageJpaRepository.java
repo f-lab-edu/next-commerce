d7ff6a5707b7d38651f4287e6f56b9c7c5df45d8
codenee <tanedoran@gmail.com>
2024-07-01 16:23:45 +0900

    [#29]jpql 쿼리문에 delete를 수행하기 위해 Modifying어노테이션 추가

--- src/main/java/org/example/nextcommerce/image/repository/jpa/ImageJpaRepository.java
+++ src/main/java/org/example/nextcommerce/image/repository/jpa/ImageJpaRepository.java
@@ -2,6 +2,7 @@ package org.example.nextcommerce.image.repository.jpa;
 
 import org.example.nextcommerce.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -11,6 +12,7 @@ public interface ImageJpaRepository extends JpaRepository<Image, Long> {
     @Query(value = "select i from Image i where i.post.id = :postId ORDER BY i.createdAt desc limit 1")
     Optional<Image> findByCreatedAtByPostId(Long postId);
 
+    @Modifying
     @Query(value = "delete from Image i where i.post.id = :postId")
     void deleteAllByPostId(Long postId);
 
