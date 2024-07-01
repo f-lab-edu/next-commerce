e7b7fe7dbb21e3da31735c2e8581a3eddd4a52d8
codenee <tanedoran@gmail.com>
2024-07-01 11:16:32 +0900

    [#29]생성 시간 컬럼 추가

--- src/main/java/org/example/nextcommerce/image/entity/Image.java
+++ src/main/java/org/example/nextcommerce/image/entity/Image.java
@@ -3,6 +3,7 @@ package org.example.nextcommerce.image.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.nextcommerce.post.entity.Post;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -33,6 +34,7 @@ public class Image {
     @Column(name = "SIZE")
     private Long fileSize;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
     public Image(Post post,String originalName, String filePath, Long fileSize){
